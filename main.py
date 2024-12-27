6d31eb329cc19cb8bff2b22d4cb4c48fb0292326
jason lian <jasonlian16@gmail.com>
2024-12-27 18:01:59 -0500

    added yellow screen to the UI

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import pygame
 
 pygame.init()
 
+# game dimension
 screen_width = 800
 screen_height = 600
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Spelling Bee Game') # caption for the game
 
 run = True
 while run:
+    yellow_screen = (255, 255, 187)
+    screen.fill(yellow_screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
