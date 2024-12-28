cd430a7ea8d0f16ed30768451b783d0448eb442d
jason lian <jasonlian16@gmail.com>
2024-12-27 21:14:43 -0500

    added button difficulties on main screen

--- button.py
+++ button.py
@@ -1,14 +1,21 @@
 import pygame 
 
 class Button():
-	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
-		self.image = image
+	def __init__(self, image, pos, text_input, font, base_color, hovering_color, scale = None):
+		if image is not None and scale is not None:
+			width = image.get_width() 
+			height = image.get_height()
+			self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		else:
+			self.image = image
+		
 		self.x_pos = pos[0]
 		self.y_pos = pos[1]
 		self.font = font
 		self.base_color, self.hovering_color = base_color, hovering_color
 		self.text_input = text_input
 		self.text = self.font.render(self.text_input, True, self.base_color)
+		
 		if self.image is None:
 			self.image = self.text
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
@@ -29,3 +36,5 @@ class Button():
 			self.text = self.font.render(self.text_input, True, self.hovering_color)
 		else:
 			self.text = self.font.render(self.text_input, True, self.base_color)
+	
+	
