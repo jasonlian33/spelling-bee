b3d85e239c59cd30aeefdcc8242330e7304172a7
jason lian <jasonlian16@gmail.com>
2024-12-29 22:54:38 -0500

    Added audio support when clicking the bee

--- noob.py
+++ noob.py
@@ -2,12 +2,13 @@ import pygame, sys
 from button import Button
 from game_manager import screen, get_font
 from pydub import AudioSegment
+from pydub.playback import play
 import random
 
 
 
 noob_dict = {
-    "snake": AudioSegment.from_file("audio/snake.m4a", format = "m4a")
+    "snake":  pygame.mixer.Sound("audio/snakew.wav")
 }
 
 
@@ -21,7 +22,7 @@ def Noob():
 
     # dimension and color of the text box 
     input_rect = pygame.Rect(250,300, 350, 50) # (x,y, x-dimension, y-dimension)
-    color_active = "White"
+    color_active = "Green"
     color_passive = "Black"
     color = color_passive
 
@@ -48,14 +49,24 @@ def Noob():
         random_word = random.choice(list(noob_dict.keys())) 
         word_audio = noob_dict[random_word] # gets the audio from the selected word 
 
+        # button for playing word_audio
+        play_audio = Button(image = pygame.image.load("assets/bee1.png"), pos = (200, 150),
+                            text_input= 'hi', font= get_font(1), base_color= "White", hovering_color= "White", scale= 0.02)
+
+        play_audio.update(screen)
+
+
         for event in pygame.event.get():
            # when playes clicks out of the tab
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-            # when player clicks on back button
+            # when player clicks on mouse button
             if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_audio.checkForInput(noob_mouse_pos):
+                        print("should be playing")
+                        word_audio.play()
                 if noob_back.checkForInput(noob_mouse_pos):
                     return
                 if input_rect.collidepoint(event.pos):
@@ -88,7 +99,8 @@ def Noob():
         screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
         input_rect.w = max(225, text_surface.get_width() + 10)
 
+        # Click and Type Message
         text1_surface = base1_font.render(type_here, True, "White")
-        screen.blit(text1_surface, (input_rect.x -50,input_rect.y - 10))
+        screen.blit(text1_surface, (input_rect.x - 175, input_rect.y + 15))
 
         pygame.display.update()
